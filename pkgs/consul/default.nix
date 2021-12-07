fb85bf6c4da29e5da6c19750ce4ddd25c9444173
John Lotoski <john.lotoski@iohk.io>
2021-12-07 16:49:48 -0600

    Bumps consul -> 1.11.0-beta3

--- pkgs/consul/default.nix
+++ pkgs/consul/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "consul";
-  version = "1.10.3";
+  version = "1.11.0-beta3";
   rev = "v${version}";
 
   # Note: Currently only release tags are supported, because they have the Consul UI
@@ -17,7 +17,7 @@ buildGoModule rec {
     owner = "hashicorp";
     repo = pname;
     inherit rev;
-    sha256 = "sha256-Jn8cF+8Wf4zZ/PFXvjCGpomSa/DvraBGW0LsZQ+Zy+4=";
+    sha256 = "sha256-84/4LoENas8YrLdz9I2MDbJn9nEYfmlKjeC0DscIpZA=";
   };
 
   patches = [
@@ -32,8 +32,8 @@ buildGoModule rec {
   # has a split module structure in one repo
   subPackages = [ "." "connect/certgen" ];
 
-  vendorSha256 = "sha256-bQWwOJj5WHFsU52Ht+BpdYeLjUaz7h1tE+IpPCzbjb4=";
-  deleteVendor = true;
+  vendorSha256 = "sha256-zAm0B9Q+HnFG47jM0OlHsI/ap3gxglxlOifhtjE6MbM=";
+  # deleteVendor = true;
 
   preBuild = ''
     buildFlagsArray+=("-ldflags"
